95e23ccbb293abf977fed4977d846454896ea7a0
Jorge Rodriguez Lopez <jrodrguez08@gmail.com>
2017-09-04 15:14:28 -0600

    Validates login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   templateUrl: 'login.component.html'
 })
+
 export class LoginComponent {
-  constructor() {
+
+  loginForm: FormGroup;
+
+  constructor(formBuilder: FormBuilder) {
+
+    this.loginForm = formBuilder.group({
+      email: [null, Validators.compose([Validators.required, Validators.email])],
+      password: [null, Validators.required]
+    });
+
   }
+
+  submitForm(value: any): void {
+
+    console.log('Form Data: ');
+    console.log(value);
+
+  }
+
 }
