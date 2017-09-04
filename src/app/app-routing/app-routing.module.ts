39d97b6f71311f7d967a6c0e1d2d45ae9ad08442
Jorge Rodriguez Lopez <jrodrguez08@gmail.com>
2017-09-03 21:17:11 -0600

    Adds sass support for app, adds login view

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from '../login/login.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
-import {AppRoutingModule, routableComponents} from './app-routing/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MdCardModule, MdButtonModule, MdInputModule } from '@angular/material';
+
+import { AppRoutingModule, routableComponents } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -16,10 +19,14 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MdCardModule,
+    MdButtonModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
