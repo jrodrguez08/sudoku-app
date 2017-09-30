0ff6fc52e68aefed7bbe4e013848aca96dfd951c
amorav90 <amorav90@gmail.com>
2017-09-29 19:59:32 -0600

    Changes in game and tile components

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-tile',
@@ -7,7 +7,7 @@ import {Component} from '@angular/core';
 
 export class TileComponent {
 
-  value = 3;
+  @Input() value = '';
 
   constructor() {
   }
